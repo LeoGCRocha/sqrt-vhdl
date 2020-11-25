e6b05c172b258a80fcf26b8b1baa7179e80d03ff
andrekuru <andrewilliam13@gmail.com>
2020-11-25 18:50:24 -0300

    Diagrams and algorithm 	new file:   Diagram - BO.png 	new file:   FSM.drawio 	new file:   sqrt.py

---
+++ sqrt.py
@@ -0,0 +1,24 @@
+def sqrt_binary_search(x):
+    
+
+    start = 0
+    end = x
+    # cont = 0
+
+    while start <= end:
+        
+        mid = (start+end)//2
+        # cont = cont + 1
+
+        if  x == mid * mid:
+            break
+        elif x < mid * mid:
+            end = mid - 1
+        elif x > mid * mid:
+            start = mid + 1
+
+    # print("cont: " + str(cont))
+    return end
+
+# while 1:
+#     print("resultado: " + str(sqrt_binary_search(int(input()))))
