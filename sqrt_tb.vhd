11527fa8dcc50d9bb4f219d779b58184840fedb2
andrekuru <andrewilliam13@gmail.com>
2020-12-02 19:25:49 -0300

    implemented cont in tb 	new file:   outputs_cycles.txt 	modified:   sqrt_tb.vhd

--- sqrt_tb.vhd
+++ sqrt_tb.vhd
@@ -14,6 +14,7 @@ architecture tb of sqrt_tb is
     signal inicio, reset, clk, pronto : std_logic;
     signal a: std_logic_vector(n-1 downto 0);
     signal saida : std_logic_vector(n-1 downto 0);
+    signal contador : natural;
 	 
 component sqrt  is
     generic (n:natural := n);
@@ -52,37 +53,52 @@ begin
 
         variable val_A: std_logic_vector(n-1 downto 0);
 
+        variable cont: natural;
+        variable write_col_to_output_cycles_buf : line;
+        file output_cycles_buf : text;
         begin
             file_open(input_buf, "/home/kuru/UFSC/SD/QuartusProjects/sqrt-vhdl/inputs.txt", read_mode);
             file_open(output_buf, "/home/kuru/UFSC/SD/QuartusProjects/sqrt-vhdl/outputs_testbench.txt", write_mode);
-				
+            file_open(output_cycles_buf, "/home/kuru/UFSC/SD/QuartusProjects/sqrt-vhdl/outputs_cycles.txt", write_mode);
 				
             -- wait until reset = '0';				
 				
             while not endfile(input_buf) loop
-                reset <= '1'; 
-                wait for clkp/2; reset <= '0';
+                reset <= '1'; inicio <= '0';
+                wait for clkp; reset <= '0';
                 readline(input_buf, read_col_from_input_buf);
                 read(read_col_from_input_buf, val_A);
 
                 a <= val_A;
 
-                wait for clkp; inicio <= '1';
-                wait for clkp; inicio <= '0';
+                wait for clkp;
+                inicio <= '1';
+                wait for clkp;
+                inicio <= '0';
+                cont := 1;
+                contador <= cont;
 
                 while (pronto = '0') loop
-                    wait for clkp;
+                    wait for clkp; cont := cont + 1;
+                    contador <= cont;
                 end loop;
                 write(write_col_to_output_buf, saida);
                 writeline(output_buf, write_col_to_output_buf);
 
+                write(write_col_to_output_cycles_buf, cont);
+                writeline(output_cycles_buf, write_col_to_output_cycles_buf);
+                wait for clkp*3;
             end loop; 
 
             write (write_col_to_output_buf, string'("Simulation from testbench completed!"));
             writeline(output_buf, write_col_to_output_buf);
 
+            write(write_col_to_output_cycles_buf, string'("Simulation from testbench completed!"));
+            writeline(output_cycles_buf, write_col_to_output_cycles_buf);
+
             file_close(input_buf);
             file_close(output_buf);
+            file_close(output_cycles_buf);
 
             wait;
         end process;
