42b6e2fb7f48eb3fe12c5b92c42f7033ad508e16
andrekuru <andrewilliam13@gmail.com>
2020-12-10 10:43:15 -0300

    Ajustes para entrega final 	renamed:    Trabalho SD.pdf -> "Arquivos adicionais de entrega/Apresenta\303\247\303\243o.pdf" 	renamed:    Relatorio.pdf -> Arquivos adicionais de entrega/Relatorio.pdf 	modified:   max_counter.py

--- max_counter.py
+++ max_counter.py
@@ -1,6 +1,16 @@
 f = open('outputs_cycles.txt', 'r')
 max = 0
+count = 0
+
 for line in f:
-    if(int(line) > max):
-        max = int(line)
-print(max)
+    try:
+        if(int(line) == max):
+            count += 1
+        if(int(line) > max):
+            max = int(line)
+            count = 1
+    except ValueError :
+        pass
+
+print("Pior caso em ciclos: " + str(max))
+print("Quantidade de pior casos: " + str(count))
